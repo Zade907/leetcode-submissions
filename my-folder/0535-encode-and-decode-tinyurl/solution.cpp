26af78bfd52f00344b47882e2e7d982b71571340
Atharva <athnivatkar908@gmail.com>
2025-01-13 09:12:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (94.34%)

---
+++ my-folder/0535-encode-and-decode-tinyurl/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
+
