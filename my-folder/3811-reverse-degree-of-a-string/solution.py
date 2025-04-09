7c7def5b32ad07be9c711248ceed0077e9f49229
Atharva <athnivatkar908@gmail.com>
2025-04-09 08:34:09 +0000

    [LeetCode Sync] Runtime - 3 ms (97.89%), Memory - 18 MB (100.00%)

---
+++ my-folder/3811-reverse-degree-of-a-string/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        sum = 0
+        for i in range(len(s)):
+            sum += (123-ord(s[i]))*(i+1)
+        return sum
+        # return 123 - ord("z")
