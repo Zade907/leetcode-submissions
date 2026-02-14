906ebd5ba19f4101c94cb289fa3fab6235c8eeac
Atharva <athnivatkar908@gmail.com>
2026-02-14 14:53:45 +0000

    [LeetCode Sync] Runtime - 11 ms (49.26%), Memory - 19.4 MB (9.33%)

---
+++ my-folder/4216-weighted-word-mapping/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        target = ""
+        for word in words:
+            charValue = 0
+            for char in word:
+                position = ord(char) - 97 
+                charValue += weights[position]
+            charValue = charValue % 26
+            target += chr(122-charValue)
+
+        return target
