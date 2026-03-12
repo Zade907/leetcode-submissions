ffbd41943eb24701c0fbd113ee88d4518f9db780
Atharva <athnivatkar908@gmail.com>
2026-03-12 17:46:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (49.56%)

--- my-folder/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/solution.py
+++ my-folder/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/solution.py
@@ -1,14 +1,6 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        oper = 0
+        count = 0 
         for i in nums:
-            if(i%3==1):
-                i -= 1 
-                oper += 1 
-            elif(i%3==2):
-                i += 1
-                oper += 1
-            else:
-                i=i
-        return oper
-            
+            if i % 3 != 0: count += 1 
+        return count 
