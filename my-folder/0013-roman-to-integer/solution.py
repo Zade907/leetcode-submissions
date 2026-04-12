cbeda88ce5079fb2575d03b8c5ea53d6c2c68c34
Atharva <athnivatkar908@gmail.com>
2026-04-12 17:59:30 +0000

    [LeetCode Sync] Runtime - 4 ms (64.01%), Memory - 19.3 MB (20.28%)

--- my-folder/0013-roman-to-integer/solution.py
+++ my-folder/0013-roman-to-integer/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_integer = {'I': 1,
-                            'V': 5,
-                            'X': 10,
-                            'L': 50,
-                            'C': 100,
-                            "D" : 500,
-                            "M": 1000}
-        number = 0
+        convert = {"I" : 1,
+        "V" : 5,
+        "X":10,
+         "L":50,
+        "C": 100,
+        "D": 500,
+        "M": 1000}
+
+        num = 0
         s = s.replace('IV','IIII') \
              .replace('IX','VIIII')  \
              .replace('XL','XXXX') \
@@ -15,5 +16,7 @@ class Solution:
              .replace('CD','CCCC') \
              .replace('CM','DCCCC')
         for char in s:
-            number += roman_to_integer[char]
-        return number
+            num += convert[char]
+        return num
+
+
