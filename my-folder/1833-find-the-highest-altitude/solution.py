99a1c68919ad6cb1cb20d6cc0739b065179f2e94
Atharva <athnivatkar908@gmail.com>
2026-06-19 15:09:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (16.32%)

--- my-folder/1833-find-the-highest-altitude/solution.py
+++ my-folder/1833-find-the-highest-altitude/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        maximum = -999
-        temp = 0
-        for i in range(0,len(gain)):
-            if(temp>maximum):
-                maximum = temp
-            temp += gain[i]
-        return max(maximum,temp)
+        maxHeight = 0 
+        currHeight = 0
+        for i in gain:
+            currHeight += i
+            maxHeight = max(maxHeight, currHeight)
+        return maxHeight
+        
